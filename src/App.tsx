4c432452b5e42390138c64f908bd355123288ba7
bestquangminh <bestquangminh@gmail.com>
2023-09-29 23:04:38 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import './App.css';
-import { Layout } from './layouts/DefautLayout'
+import { Layout } from './layouts/DefautLayout';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import routes from './routes';
 function App() {
   return (
-    <Router>
-      <div className="app">
+    <Router basename={process.env.PUBLIC_URL}>
+      <div className='app'>
         <Routes>
           {routes.map((route, index) => {
             const Page: () => JSX.Element = route.component;
-            return <Route key={index} path={route.path} element={<Layout><Page /></Layout>} />
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
           })}
         </Routes>
       </div>
